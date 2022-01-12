b5f82d20047b02eeadf2306e2d44426855db0f5b
Marcos Barbosa <marcos.barbosa@unfei.edu.br>
2022-01-12 10:49:01 -0300

    Adicionando arquivos finais ts

---
+++ src/components/Comentarista.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="comentar">Comentar</button>
+    <p>{{ comentario }}</p>
+    <p>{{ detalhes }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+import { Prop } from "vue-property-decorator";
+
+@Component
+export default class Comentarista extends Vue {
+  private comentario = "";
+  @Prop() private nome?: string;
+
+  private comentar(): void {
+    this.comentario = "Olá Mundo!!!";
+  }
+
+  get detalhes(): string {
+    return `${this.nome} está comentando`;
+  }
+}
+</script>
+
+<style></style>
